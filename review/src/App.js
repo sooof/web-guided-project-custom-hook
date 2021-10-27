c45c6efcb91efd7de8c6c222dbc13582a1cff7c5
sooof <sooof@me.com>
2021-10-27 14:31:58 -0700

    add components/PokeDetails.js

--- review/src/App.js
+++ review/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./styles.scss";
 import axios from 'axios';
 import data from "../data";
+import PokeDetails from './components/PokeDetails'
 
 function App() {
   const [pokemen, setPokemen] = useState([]);
@@ -20,27 +21,7 @@ function App() {
 
   return (
     <div className="App">
-      <div id="selectedDiv">
-        <h2>Selected Pokemon: {selectedPokemon.name || "none"}</h2>
-        {selectedPokemon.name && (
-          <div>
-            <img
-              src={selectedPokemon.sprites.front_default}
-              alt={selectedPokemon.sprites.front_default}
-            />
-            <h5>Height: {selectedPokemon.height}</h5>
-            <h5>Weight: {selectedPokemon.weight}</h5>
-            <h5>Abilities:</h5>
-            <ul>
-              {selectedPokemon.abilities.map((a) => (
-                <li key={a.ability.name}>
-                  {a.ability.name} - Slot {a.slot}{" "}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
+      <PokeDetails selectedPokemon={selectedPokemon}/>
       <div id="pokeList">
         {pokemen.map((pokemon) => (
           <div
