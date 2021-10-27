a96b266d9c352aaf2e17e0cc89ebdcfc1f876757
sooof <sooof@me.com>
2021-10-27 14:39:57 -0700

    add components/PokeList.js

--- review/src/App.js
+++ review/src/App.js
@@ -3,6 +3,8 @@ import "./styles.scss";
 import axios from 'axios';
 import data from "../data";
 import PokeDetails from './components/PokeDetails'
+import PokeList from './components/PokeList'
+
 
 function App() {
   const [pokemen, setPokemen] = useState([]);
@@ -22,22 +24,7 @@ function App() {
   return (
     <div className="App">
       <PokeDetails selectedPokemon={selectedPokemon}/>
-      <div id="pokeList">
-        {pokemen.map((pokemon) => (
-          <div
-            onClick={() => handlePoke(pokemon.id)}
-            key={pokemon.id}
-            className="pokemon"
-          >
-            <img src={pokemon.img} alt={pokemon.name} />
-            <div>
-              <h3>{pokemon.name}</h3>
-              {pokemon.next_evolution &&
-                pokemon.next_evolution.map((e) => <p key={e.num}>{e.name}</p>)}
-            </div>
-          </div>
-        ))}
-      </div>
+      <PokeList pokemen={pokemen} handlePoke={handlePoke}/>
     </div>
   );
 }
