0458bd0a3f9ac5e0f3fb4fd25e3512a92f812563
sooof <sooof@me.com>
2021-12-05 20:50:01 -0800

    Review : array

--- review/src/App.js
+++ review/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./styles.scss";
 
+const func = () => {
+  return "here";
+}
+const array = [1,2,3,4,5]
+const [first, second, third] = array;
+console.log(first, second, third)
 
 function App() {
-
-
   return (
     <div className="App">
       <h1>Hello CodeSandBox</h1>
