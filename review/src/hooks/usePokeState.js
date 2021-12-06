351a243640a84b293a6d7b686072126e208574c6
sooof <sooof@me.com>
2021-12-05 23:56:15 -0800

    review: hooks getPokemon

--- review/src/hooks/usePokeState.js
+++ review/src/hooks/usePokeState.js
@@ -2,6 +2,9 @@ import {useState, useEffect} from 'react';
 import getPokemen from './../services/getPokemen';
 import axios from 'axios';
 
+const getPokemon =(id) =>{
+  return   axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+}
 const usePokeState = () => {
     const [pokemen, setPokemen] = useState([]);
     const [selectedPokemon, setSelectedPokemon] = useState({});
@@ -11,10 +14,16 @@ const usePokeState = () => {
     }, []);
   
     const handlePoke = (id) => {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+      console.log(getPokemon(id))
+      getPokemon(id)
         .then((res) => {
+          console.log(res)
           setSelectedPokemon(res.data);
         });
+      // axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+      //   .then((res) => {
+      //     setSelectedPokemon(res.data);
+      //   });
     };
   
     return [pokemen, handlePoke, selectedPokemon];
